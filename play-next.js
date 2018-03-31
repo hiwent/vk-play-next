f63147c44db563a5f41a2097d67af1ff08c6d807
Stanislav Gazul <Sgazul@gmail.com>
2018-03-31 16:50:01 +0300

    Update play-next.js

--- play-next.js
+++ play-next.js
@@ -1,1 +1,25 @@
+// ==UserScript==
+// @name         Play next
+// @version      0.1
+// @description  Autoclicker for the PlayNext button in audioplayer at vk.com
+// @author       hiwent, https://github.com/hiwent
+// @match        *INSERT HERE FULL LINK TO TARGET AUDIOLIST AT VK.COM, eg: https://vk.com/audios<yourVKid>*
+// @downloadURL  https://github.com/hiwent/vk-play-next/blob/master/play-next.js
+// @updateURL    https://github.com/hiwent/vk-play-next/blob/master/play-next.js
+// @grant GM_getValue
+// @grant unsafeWindow
+// ==/UserScript==
+
+var i=0;
+var k=1000; // Here you can set the quantity of clicks
+function next() {
+    if(i<k){
+        i++;
+        document.getElementsByClassName("audio_page_player_next")[0].click();
+    } else{
+    clearInterval (timerVar);
+    timerVar = "";
+    }
+}
+var timerVar = setInterval(next, 5000); // Here you can set the time for pause between clicks. Here it's set to 5 sec.
 
