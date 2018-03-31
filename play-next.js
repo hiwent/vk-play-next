ec1feb900b04f7e8ceb90994379a0c41e3d116e0
Stanislav Gazul <Sgazul@gmail.com>
2018-03-31 16:53:33 +0300

    Update play-next.js

--- play-next.js
+++ play-next.js
@@ -3,7 +3,7 @@
 // @version      0.1
 // @description  Autoclicker for the PlayNext button in audioplayer at vk.com
 // @author       hiwent, https://github.com/hiwent
-// @match        *INSERT HERE FULL LINK TO TARGET AUDIOLIST AT VK.COM, eg: https://vk.com/audios<yourVKid>*
+// @match        *INSERT HERE FULL LINK TO THE TARGET AUDIOLIST AT VK.COM, eg: https://vk.com/audios<yourVKid>*
 // @downloadURL  https://github.com/hiwent/vk-play-next/blob/master/play-next.js
 // @updateURL    https://github.com/hiwent/vk-play-next/blob/master/play-next.js
 // @grant GM_getValue
